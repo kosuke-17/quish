eec0953cbbcb5288a634f6369ad3fc079847a28a
kosuke-17 <tamutamu17@icloud.com>
2022-01-07 16:12:30 +0900

    トップページのコンポーネントをリプレイス終了

--- src/templates/index.tsx
+++ src/templates/index.tsx
@@ -1,2 +1,4 @@
 // pagesから渡ってきたデータをもとに処理を行う
 export { default as LoginForm } from "./LoginFrom";
+export { default as ArticleList } from "./ArticleList";
+export { default as Profile } from "./Profile";
