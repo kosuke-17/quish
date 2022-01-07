4611f6a36e6abed94025389d7c42fba14900e6b4
kosuke-17 <tamutamu17@icloud.com>
2022-01-07 14:27:48 +0900

    レイアウトコンポーネント作成

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,4 +1,5 @@
 import { Layout } from "antd";
+import FooterComp from "./FooterComp";
 import HeaderComp from "./organisms/HeaderComp";
 
 const { Header, Footer } = Layout;
@@ -6,13 +7,13 @@ const { Header, Footer } = Layout;
 // レイアウト全体のスタイル
 const LayoutComp: React.FC = ({ children }) => {
   return (
-    <Layout>
+    <Layout className="h-screen">
       <Header className="bg-blue-500 w-full h-16 relative">
         <HeaderComp />
       </Header>
       <Layout>{children}</Layout>
       <Footer className="bg-blue-500 h-12 text-center">
-        <div className="text-white p-2"> © 2011-2021 Quish Inc.</div>
+        <FooterComp />
       </Footer>
     </Layout>
   );
