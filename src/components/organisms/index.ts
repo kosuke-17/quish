52bdb853674f39a1aafc599cec5c011ecc051e5a
kosuke-17 <tamutamu17@icloud.com>
2022-01-12 22:06:51 +0900

    編集

--- src/components/organisms/index.ts
+++ src/components/organisms/index.ts
@@ -3,4 +3,5 @@ export { default as ArticleComp } from "./ArticleComp";
 export { default as HeaderComp } from "./HeaderComp";
 export { default as LoginUserComp } from "./LoginUserComp";
 export { default as ProfileComp } from "./ProfileComp";
+export { default as RectangleProfile } from "./RectangleProfile";
 export { default as RegisterUserComp } from "./RegisterUserComp";
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,21 @@
+import Router from "next/router";
 import { ArticleList, Profile } from "../templates";
+
+const goToProfile = () => {
+  Router.push("/profile");
+};
 
 const Home: React.FC = () => {
   return (
-    <div className="flex">
-      <div className="w-2/6 h-screen bg-gray-100">
+    <div className="flex h-screen bg-orange-100	">
+      <div className="w-2/6">
         <Profile />
       </div>
-      <div className="w-4/6 h-screen bg-gray-100 overflow-y-auto">
+      <div className="w-4/6 overflow-y-auto">
         <ArticleList />
       </div>
-      <div className="w-2/6 h-screen bg-gray-100">
+      <button onClick={goToProfile}>プロフィールへ</button>
+      <div className="w-2/6">
         <button
           type="button"
           className="py-2 px-5 m-4 bg-white text-blue-500 border-2 border-blue-500 focus:ring-blue-500 focus:ring-offset-blue-400 w-11/12 transition ease-in duration-200 text-center text-base font-semibold shadow-md hover:bg-blue-500 hover:text-white rounded-xl"
