72d5e75330c08cefcb89d4304689c24ee7551f0f
kosuke-17 <tamutamu17@icloud.com>
2022-01-12 14:08:31 +0900

    pagesのディレクトリ削除

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,8 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import LayoutComp from "../components/Layout";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <LayoutComp>
-      <Component {...pageProps} />
-    </LayoutComp>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
