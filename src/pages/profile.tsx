635dc6517ca7f2f39f3224b4c13b2324058912d6
kosuke-17 <tamutamu17@icloud.com>
2022-01-13 10:04:19 +0900

    タイムラインの編集

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { RectangleProfile } from "../components/organisms";
-import { TrademarkCircleTwoTone } from "@ant-design/icons";
+import { TrademarkCircleTwoTone, LoadingOutlined } from "@ant-design/icons";
 import { Input, Timeline } from "antd";
 const { TextArea } = Input;
 const { Item } = Timeline;
@@ -29,6 +29,10 @@ const profile: React.FC = () => {
               />
             </span>
           </div>
+        </div>
+      </div>
+      <div className="flex justify-center">
+        <div className=" w-1/2">
           <div className="block mt-10">
             <Timeline mode="right">
               <div className="flex">
@@ -37,13 +41,45 @@ const profile: React.FC = () => {
                   dot={
                     <TrademarkCircleTwoTone
                       twoToneColor="#FFB545"
+                      className="text-3xl rounded-2xl"
+                    />
+                  }
+                >
+                  将来
+                </Item>
+                <div className="ml-6 w-full">
+                  <p className="font-bold">この先やってみたいこと</p>
+                  <div className="text-center w-full rounded-2xl hover:bg-gray-100">
+                    <TextArea
+                      placeholder="【フルスタックエンジニアになる】私は学習意欲が他の人よりも高いと考えられる。そのため、知識やスキルを増やして、仕事で活躍できるようなフルスタックエンジニアを目標にすることが良いと思ったから。"
+                      autoSize={{ minRows: 5 }}
+                      bordered={false}
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className="my-4 text-center divide-y-2 divide-dashed">
+                <button className="p-2 rounded-xl bg-[rgb(255,195,98)] hover:bg-[rgb(255,207,131)] drop-shadow-2xl">
+                  新しいプロジェクト・学習言語
+                </button>
+                {/* ボタンの下の点線 */}
+                <div className="relative bottom-5 -z-20"></div>
+              </div>
+              <div className="flex">
+                <Item
+                  className="w-44 text-gray-500 text-xl pr-10"
+                  dot={
+                    <TrademarkCircleTwoTone
+                      twoToneColor="#FFB545"
                       className="text-3xl rounded-full"
                     />
                   }
                 >
-                  2015年9月
+                  2015年10月
+                  {/* <br /> - <br /> */}
+                  2015年11月
                 </Item>
-                <div className="ml-3 bg-slate-400 w-full">
+                <div className="ml-6 mb-5 bg-slate-400 w-full">
                   <p>タイトルとアピールなど</p>
                   <div className="bg-slate-200 w-full">
                     アピールの内容(現場で使用している言語や意識していること)
@@ -63,27 +99,7 @@ const profile: React.FC = () => {
                 >
                   2015年9月
                 </Item>
-                <div className="ml-3 bg-slate-400 w-full">
-                  <p>タイトルとアピールなど</p>
-                  <div className="bg-slate-200 w-full">
-                    アピールの内容(現場で使用している言語や意識していること)
-                  </div>
-                  <div className="bg-slate-300 w-full">ここにArticleコンプ</div>
-                </div>
-              </div>
-              <div className="flex">
-                <Item
-                  className="w-44 text-gray-500 text-xl pr-10"
-                  dot={
-                    <TrademarkCircleTwoTone
-                      twoToneColor="#FFB545"
-                      className="text-3xl rounded-full"
-                    />
-                  }
-                >
-                  2015年9月
-                </Item>
-                <div className="ml-3 bg-slate-400 w-full">
+                <div className="ml-6 mb-5 bg-slate-400 w-full">
                   <p>タイトルとアピールなど</p>
                   <div className="bg-slate-200 w-full">
                     アピールの内容(現場で使用している言語や意識していること)
