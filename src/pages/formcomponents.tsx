2b5c139828cac4adf31bbb400795d839c6da75eb
kosuke-17 <tamutamu17@icloud.com>
2022-01-14 09:03:44 +0900

    モーダル作成

--- src/pages/formcomponents.tsx
+++ src/pages/formcomponents.tsx
@@ -2,9 +2,10 @@ import { Input, Form, Button, Select, DatePicker } from "antd";
 import TextArea from "antd/lib/input/TextArea";
 import React from "react";
 import moment from "moment";
+import ModalScreen from "../components/ModalScreen";
 const { Option } = Select;
 
-const formcomponents = () => {
+const FormComponents: React.FC = () => {
   const DATEFORMAT = "YYYY年M月";
   const NOWTIME = new Date();
   const langueges = [
@@ -24,6 +25,7 @@ const formcomponents = () => {
     "flutter",
   ];
   const lang_children = [];
+  const engineer_types = ["FR", "WEB", "ML", "CL", "QA"];
   for (const languege of langueges) {
     lang_children.push(
       <Option key={languege} value={languege}>
@@ -34,7 +36,79 @@ const formcomponents = () => {
   return (
     <div className="h-screen">
       <div className="flex-none">フォームコンポーネント(削除用)</div>
-      <div className="flex justify-center items-center h-screen">
+      <div className="fixed inset-0 z-50 flex justify-center items-center h-screen">
+        <div className="flex justify-center bg-white pt-4 h-auto w-96 rounded-lg border shadow-md ">
+          <Form>
+            <div>
+              <div className="p-2 text-3xl font-bold">ユーザー情報編集</div>
+              <div className="flex items-center text-xl">
+                <div className="w-20 h-20 rounded-full bg-slate-300"></div>
+              </div>
+              <div className="text-xl mt-2 p-2 rounded-xl">
+                <Form.Item
+                  name="username"
+                  rules={[{ required: true, message: "使用技術が空欄です" }]}
+                >
+                  <Input
+                    className="text-3xl focus:placeholder-gray-600"
+                    placeholder="山田太郎"
+                    bordered={false}
+                  />
+                </Form.Item>
+              </div>
+              <div className="text-xl mt-2 p-2 rounded-xl">
+                <Form.Item
+                  name="mailaddress"
+                  rules={[{ required: true, message: "使用技術が空欄です" }]}
+                >
+                  <Input
+                    className="text-3xl focus:placeholder-gray-600"
+                    placeholder="メールアドレス"
+                    bordered={false}
+                  />
+                </Form.Item>
+              </div>
+              <div className="mt-2">
+                <Form.Item
+                  name="engineertype"
+                  rules={[{ required: true, message: "使用技術が空欄です" }]}
+                >
+                  <Select defaultValue="FR" className="w-20">
+                    {engineer_types.map((engineer) => {
+                      return (
+                        <Option key={engineer} value={engineer}>
+                          {engineer}
+                        </Option>
+                      );
+                    })}
+                  </Select>
+                </Form.Item>
+              </div>
+
+              <Form.Item wrapperCol={{ offset: 13, span: 16 }}>
+                <div className="flex mt-2">
+                  <Button size="large" shape="round" htmlType="button">
+                    戻る
+                  </Button>
+                  &nbsp;
+                  <Button size="large" shape="round" htmlType="submit">
+                    保存
+                  </Button>
+                </div>
+              </Form.Item>
+            </div>
+          </Form>
+        </div>
+      </div>
+      <ModalScreen />
+    </div>
+  );
+};
+
+export default FormComponents;
+
+{
+  /* <div className="fixed inset-0 z-50 flex justify-center items-center h-screen">
         <div className="flex justify-center bg-white pt-8 h-auto w-96 rounded-lg border shadow-md ">
           <Form className="w-5/6">
             <Form.Item
@@ -119,10 +193,5 @@ const formcomponents = () => {
             </Form.Item>
           </Form>
         </div>
-      </div>
-      <div className="opacity-25 fixed inset-0 -z-40 bg-black" />
-    </div>
-  );
-};
-
-export default formcomponents;
+      </div> */
+}
