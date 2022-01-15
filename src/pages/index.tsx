2a219144ed6c84083db9e3facba29802a2292c63
kosuke-17 <tamutamu17@icloud.com>
2022-01-15 15:06:21 +0900

    画面遷移のメソッド作成

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,9 @@ import { ArticleList, Profile } from "../templates";
 const goToProfile = () => {
   Router.push("/profile");
 };
+const addArticle = () => {
+  Router.push("/articleAdd");
+};
 
 const Home: React.FC = () => {
   return (
@@ -18,6 +21,7 @@ const Home: React.FC = () => {
       <div className="w-2/6">
         <button
           type="button"
+          onClick={addArticle}
           className="py-2 px-5 m-4 bg-white text-blue-500 border-2 border-blue-500 focus:ring-blue-500 focus:ring-offset-blue-400 w-11/12 transition ease-in duration-200 text-center text-base font-semibold shadow-md hover:bg-blue-500 hover:text-white rounded-xl"
         >
           記事を投稿する
