4a249bb26fbb0d4e523ad16262db69b7b6997b19
kosuke-17 <tamutamu17@icloud.com>
2022-01-15 10:41:07 +0900

    プロフィール作成

--- src/components/organisms/ProfileComp.tsx
+++ src/components/organisms/ProfileComp.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import SkillTagsOnProfile from "../molecules/SkillTagsOnProfile";
+import Image from "next/image";
 
 // FCの型定義
 type Props = {
@@ -20,34 +20,79 @@ type Props = {
 };
 
 const ProfileComp: React.FC<Props> = ({ user_info_data }) => {
+  const tagStyle =
+    "mx-1 mb-1 p-1 bg-[rgb(255,195,98)] text-white text-center font-sans text-xs shadow-md rounded-lg";
+  const tagsName = [
+    "フロントエンド",
+    "CSS",
+    "tailwindCSS",
+    "tailwindCSS",
+    "初心者",
+    "フロントエンド",
+    ,
+    "初心者",
+    "tailwindCSS",
+    "初心者",
+    "フロントエンド",
+  ];
   return (
     <div className="w-full p-2 m-2 bg-white rounded-lg border shadow-md">
       <div className="m-4">
         <div className="flex justify-center items-center">
-          <div className="w-20 h-20 rounded-full bg-slate-300"></div>
-          <div className="pl-3">@{user_info_data.user_name}</div>
+          {/* image(User) */}
+          <Image
+            className="rounded-full"
+            src={"/img/avatar.jpg"}
+            alt="アバター"
+            width={90}
+            height={90}
+          />
+          {/* userName(User) */}
+          <div className="pl-3 text-center">
+            <div>@{user_info_data.user_name}</div>
+            <div className="mt-2 p-1 rounded-full text-white bg-[rgb(255,195,98)] hover:bg-[rgb(255,207,131)]">
+              <button>フォロー</button>
+            </div>
+          </div>
         </div>
         <div className="m-4 flex jusify-around divide-x divide-black ">
           <div className="flex-grow text-center">
+            {/*  */}
             投稿数
             <div>11</div>
           </div>
           <div className="flex-grow text-center">
+            {/*  */}
             フォロー
             <div>122</div>
           </div>
           <div className="flex-grow text-center ">
+            {/*  */}
             フォロワー
             <div>140</div>
           </div>
         </div>
         <div className="text-xl mt-2 flex justify-center items-center">
           職種:
-          <span className="ml-4 px-3 rounded-md text-white bg-orange-400">
+          <span className="ml-4 px-3 rounded-md text-white bg-[rgb(255,195,98)]">
+            {/* engineerType(User) */}
             FR
           </span>
         </div>
+        <div className="mt-2">
+          {/* tags(User) */}
+          <div className="flex flex-wrap">
+            {tagsName.map((tagName) => {
+              return (
+                <span className={tagStyle} key={tagName}>
+                  {tagName}
+                </span>
+              );
+            })}
+          </div>
+        </div>
       </div>
+      {/* comment(User) */}
       <div className="block m-2">{user_info_data.comment}</div>
     </div>
   );
